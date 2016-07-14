bd32cf998e6d518aac87432d6fa5db2ad408871c
Dao Le Quang <daolq@asiantech.vn>
2016-07-14 11:25:42 +0700

    init

--- app/src/main/java/vn/fstyle/androiddemosocial/MainActivity.java
+++ app/src/main/java/vn/fstyle/androiddemosocial/MainActivity.java
@@ -1,7 +1,8 @@
 package vn.fstyle.androiddemosocial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(View v) {
+        switch (v.getId()) {
+            case R.id.btnFacebook:
+                break;
+            case R.id.btnTwitter:
+                break;
+            case R.id.btnGooglePlus:
+                break;
+            case R.id.btnLine:
+                break;
+        }
     }
 }
